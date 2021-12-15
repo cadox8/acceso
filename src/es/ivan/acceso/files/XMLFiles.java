f206ae3445e44fac570328583950607afe4535ca
Iván (cadox8) <cadox8@gmail.com>
2021-12-15 21:14:33 +0100

    idk

--- src/es/ivan/acceso/files/XMLFiles.java
+++ src/es/ivan/acceso/files/XMLFiles.java
@@ -10,6 +10,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 public class XMLFiles extends AbstractFile {
 
@@ -54,7 +55,87 @@ public class XMLFiles extends AbstractFile {
         }
     }
 
+    public void writeNewXML(String fileName, String parentNode, Scanner scanner) {
+        final File file = this.getFile(fileName);
 
+        if (!file.exists()) {
+            try {
+                final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                final Document document = factory.newDocumentBuilder().newDocument();
+
+                final Element root = document.createElement(parentNode);
+
+                String line;
+                while (!(line = scanner.nextLine()).equalsIgnoreCase("listo")) {
+
+                }
+            } catch (ParserConfigurationException e) {
+                Log.error("El archivo no existe o no está bien formado");
+                Log.stack(e.getStackTrace());
+            }
+        } else {
+            Log.error("Ya existe un archivo llamado " + fileName + ".xml");
+        }
+    }
+
+    /**
+     * Ask4Child, return parent with child / siblings
+     *
+     * @param document
+     * @param parent
+     * @param line
+     * @return
+     */
+    private Element ask4Child(Document document, Element parent, String line) {
+        Element element;
+
+        // Formato: key(atrib): value || key(atrib)
+        final String parsedXML = line.replaceAll("\\s", ""); // Borramos todos los espacios
+
+        if (parsedXML.contains(":")) {
+            // --- Atributos ---
+            final HashMap<String, String> attributes = new HashMap<>();
+            final String[] attributesParsed = parsedXML.split("@\"\\[(.+?)\\]\"")[0].split("=");
+
+            for (int i = 0; i < attributesParsed.length - 1; i += 2) {
+                if (attributesParsed[i] == null) attributesParsed[i] = "";
+                if (attributesParsed[i + 1] == null) attributesParsed[i + 1] = null;
+
+                attributes.put(attributesParsed[i], attributesParsed[i + 1]);
+            }
+            // ---  ---
+
+            final String[] realElement = parsedXML.replaceAll("@\"(.+?)\"", "").split(":");
+            if (realElement[1] == null) {
+                realElement[1] = "";
+                Log.warning("Elemento sin valor.");
+            }
+
+            element = document.createElement(realElement[0]);
+            element.setNodeValue(realElement[1]);
+            attributes.forEach(element::setAttribute);
+
+            parent.appendChild(element);
+
+            Log.normal("¿Añadir hermano? [S/N]");
+
+        } else {
+            // Elemento con hijos
+
+            element = null;
+
+            parent = null; //new element from here
+        }
+
+        return parent;
+    }
+
+    /**
+     * Muestra los hijos de cada Nodo
+     *
+     * @param parent El Nodo padre
+     * @param tabs Las tabulaciones a pasar para imprimir
+     */
     private void showChildren(@NonNull Node parent, int tabs) {
         if (!parent.hasChildNodes()) return;
         final NodeList children = parent.getChildNodes();
@@ -96,6 +177,12 @@ public class XMLFiles extends AbstractFile {
         }
     }
 
+    /**
+     * Genera las tabulaciones requeridas
+     *
+     * @param tabs Las tabulaciones a generar
+     * @return Un String con las tabulaciones generadas
+     */
     private String generateSpacer(int tabs) {
         final StringBuilder sb = new StringBuilder();
 
